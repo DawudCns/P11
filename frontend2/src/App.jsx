faee0775e1beb8ac295b5b33eeda0b2eac9ae21d
Dawud Cns <davidhc@hotmail.be>
2024-05-16 16:49:46 +0200

    changement

---
+++ frontend2/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
+import Error from "./pages/Error/Error";
+// import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+// import ProtectedRoute from "./protectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend2/src/pages/Profile/Profile.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react"; // Importe React et useState depuis le module "react"
+
+const Profile = () => {
+  return <div>Profil</div>;
+};
+
+export default Profile; // Exporte le composant
